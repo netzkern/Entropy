e2947a03c1145c86ac008cb40204da1927471c76
Florian Koch <florian.koch@netzkern.de>
2016-09-19 10:15:51 +0200

    TFS PR script 1.2.0 - work item details

--- userscripts/tfs_pull_request_improvements.user.js
+++ userscripts/tfs_pull_request_improvements.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TFS Pull Request improvements
 // @namespace    de.netzkern
-// @version      1.1.0
+// @version      1.2.0
 // @description  Some TFS improvements.
 // @author       Florian Koch . netzkern
 // @match        *://backlog.netzkern.de/tfs/*/pullrequest*
@@ -14,6 +14,7 @@
 'use strict';
 
 // changelog:
+// 1.2.0 - Work item information
 // 1.1.0 - markdown support
 // 1.0.2 - removed blue border on PRs where user was added but you have not voted yet.
 // 1.0.1 - removed console.log... :)
@@ -23,54 +24,84 @@ var mdCSS = GM_getResourceText("mdCSS");
 GM_addStyle (mdCSS);
 
 // CSS styling
-GM_addStyle ( "                                     \
-    .vc-pullrequest-entry.reviewed {                \
-        outline: 1px solid #007acc;                 \
-    }                                               \
-    .reviewer-image {                               \
-        margin: 2px;                                \
-        width: 20px;                                \
-        height: 20px;                               \
-        outline: 2px solid transparent;             \
-    }                                               \
-    .reviewer-image.me {                            \
-        /*outline: 2px solid #007acc;*/             \
-    }                                               \
-    .merge {                                        \
-        color: #0a0;                                \
-        font-weight: bold;                          \
-    }                                               \
-    .merge.failed {                                 \
-        color: red;                                 \
-    }                                               \
-    .status {                                       \
-        font-weight: bold;                          \
-    }                                               \
-    .status.pos {                                   \
-        color: #0a0;                                \
-    }                                               \
-    .status.neg {                                   \
-        color: red;                                 \
-    }                                               \
-                                                    \
-    .markdown-body {                                \
-        font-size: inherit;                         \
-        line-height: inherit;                       \
-        color: black;                               \
-        font-family: inherit;                       \
-    }                                               \
-    .markdown-body ul, .markdown-body ol {          \
-        font-size: 0;                               \
-    }                                               \
-    .markdown-body li {                             \
-        font-size: 12px;                            \
-    }                                               \
+GM_addStyle ( "                                             \
+    .vc-pullrequest-entry.reviewed {                        \
+        outline: 1px solid #007acc;                         \
+    }                                                       \
+    .reviewer-image {                                       \
+        margin: 2px;                                        \
+        width: 20px;                                        \
+        height: 20px;                                       \
+        outline: 2px solid transparent;                     \
+    }                                                       \
+    .reviewer-image.me {                                    \
+        /*outline: 2px solid #007acc;*/                     \
+    }                                                       \
+    .merge {                                                \
+        color: #0a0;                                        \
+        font-weight: bold;                                  \
+    }                                                       \
+    .merge.failed {                                         \
+        color: red;                                         \
+    }                                                       \
+    .status {                                               \
+        font-weight: bold;                                  \
+    }                                                       \
+    .status.pos {                                           \
+        color: #0a0;                                        \
+    }                                                       \
+    .status.neg {                                           \
+        color: red;                                         \
+    }                                                       \
+                                                            \
+    .markdown-body {                                        \
+        font-size: inherit;                                 \
+        line-height: inherit;                               \
+        color: black;                                       \
+        font-family: inherit;                               \
+    }                                                       \
+    .markdown-body ul, .markdown-body ol {                  \
+        font-size: 0;                                       \
+    }                                                       \
+    .markdown-body li {                                     \
+        font-size: 12px;                                    \
+    }                                                       \
+    .workitem-description-details {                         \
+        height: 50px;                                       \
+        overflow: hidden;                                   \
+        position: relative;                                 \
+        padding-bottom: 18px;                               \
+    }                                                       \
+    .workitem-description-details:after {                   \
+        content: '... hover for more details';              \
+        display: block;                                     \
+        position: absolute;                                 \
+        bottom: 0px;                                        \
+        height: 14px;                                       \
+        width: 100%;                                        \
+        padding-top: 4px;                                   \
+        background: white;                                  \
+    }                                                       \
+    .workitem-description-details:hover {                   \
+        height: auto;                                       \
+    }                                                       \
+    .workitem-description-details:hover:after {             \
+        display: none;                                      \
+    }                                                       \
+    .work-item-summary table {                              \
+        border-collapse: separate;                          \
+        border-spacing: 5px;                                \
+    }                                                       \
+    .vc-pullrequest-work-items-container:not(:last-child) { \
+        border-bottom: 1px solid lightgrey;                 \
+     }                                                      \
 " );
 
 var currentIdentity;
 var $rightPane;
 var $prEntries;
 var prDesc;
+var workItemsData;
 var md = new Remarkable();
 
 function renderPrInfo(line, data) {
@@ -87,7 +118,7 @@ function renderPrInfo(line, data) {
             score += reviewer.vote;
             reviewers += '<img class="reviewer-image' + (iReviewed ? ' me' : '') + '" title="' + reviewer.displayName + '" src="' + reviewer.imageUrl + '" />';
         }
-        
+
         var $prTitle = $prEntries.find('.vc-pullrequest-entry-title[href*="/' + data.pullRequestId + '#"]');
         var $pr = $prTitle.closest('.vc-pullrequest-entry');
         if(iReviewed) {
@@ -95,7 +126,7 @@ function renderPrInfo(line, data) {
         }
         var prReviewers = '<td rowspan="2">reviewers:<br>' + reviewers + '</td>';
         $prTitle.parent().removeAttr('colspan').after(prReviewers);
-        
+
         var scoreClass = '';
         if(score > 0) {
             scoreClass = 'pos';
@@ -137,6 +168,91 @@ function renderMarkdown() {
     }
 }
 
+function parseParams(str) {
+    return str.split('&').reduce(function (params, param) {
+        var paramSplit = param.split('=').map(function (value) {
+            return decodeURIComponent(value.replace('+', ' '));
+        });
+        params[paramSplit[0]] = paramSplit[1];
+        return params;
+    }, {});
+}
+
+function tableWrap(key, val, disableHhtmlEncode) {
+    if(val) {
+        if(!disableHhtmlEncode) {
+            val = val.toHtmlEntities();
+        }
+        return '<tr><td>' + key + ':</td><td>'
+            + val
+            + '</td></tr>';
+    }
+    return '';
+}
+
+/**
+ * Convert a string to HTML entities
+ */
+String.prototype.toHtmlEntities = function() {
+    return this.replace(/./gm, function(s) {
+        return "&#" + s.charCodeAt(0) + ";";
+    });
+};
+
+/**
+ * Create string from HTML entities
+ */
+String.fromHtmlEntities = function(string) {
+    return (string+"").replace(/&#\d+;/gm,function(s) {
+        return String.fromCharCode(s.match(/\d+/gm)[0]);
+    })
+};
+
+function renderWorkItemsDetails() {
+    if(workItemsData && workItemsData.length) {
+        $('.vc-pullrequest-work-items-container').each(function(index) {
+            var $this = $(this);
+            var workItemData = workItemsData[index];
+            //console.log(workItemData);
+            var $url = $this.find('[data-bind="text: $data.itemId, attr: {href: $data.url}"]');
+            var $title = $this.find('[data-bind="text: $data.title"]');
+            var $status = $this.find('[data-bind="text: $data.fullStatus"]');
+            var href = $url.attr('href');
+            href = href.substring(0, href.lastIndexOf('/'));
+            href += '/' + workItemData.id;
+            $url.attr('href', href).html(workItemData.fields['System.WorkItemType'] + ' ' + workItemData.id);
+            $title.html(workItemData.fields['System.Title']);
+
+            var lane = '';
+            var column = '';
+            for(var key in workItemData.fields) {
+                if(key.indexOf('_Kanban.Column') >= 0 && key.indexOf('_Kanban.Column.Done') < 0) {
+                    column = workItemData.fields[key];
+                } else if(key.indexOf('_Kanban.Lane') >= 0) {
+                    lane = workItemData.fields[key];
+                }
+            }
+
+            var description = '';
+            if(workItemData.fields['System.Description']) {
+                description = '<div class="workitem-description-details">' + workItemData.fields['System.Description'] + '</div>';
+            }
+
+            var status = '<table>';
+            status += tableWrap('State', workItemData.fields['System.State']);
+            status += tableWrap('Area', workItemData.fields['System.AreaPath']);
+            status += tableWrap('Lane', lane);
+            status += tableWrap('Column', column);
+            status += tableWrap('Tags', workItemData.fields['System.Tags']);
+            status += tableWrap('Assigned to', workItemData.fields['System.AssignedTo']);
+            status += tableWrap('Description', description, true);
+            status += '</table>';
+
+            $status.html(status);
+        });
+    }
+}
+
 if(window.location.pathname.indexOf('pullrequests') >= 0) { // PR overview
     $(document).ajaxComplete(function(event, request, settings) { // trigger after moving tasks
         if(settings && settings.url && settings.url.indexOf('pullRequests?status=') >= 0 && settings.url.indexOf('pullRequests?status=1') < 0 && request.responseJSON) {
@@ -148,12 +264,26 @@ if(window.location.pathname.indexOf('pullrequests') >= 0) { // PR overview
         }
     });
 } else { // PR detail
+    $.ajaxPrefilter(function( options, originalOptions, jqXHR ) {
+        // Modify ajax request to return all fields... definitely not a hack :D
+        if(options && options.url && options.url.indexOf('_apis/wit/workItems') >= 0) {
+            var parsedData = parseParams(options.data);
+            delete parsedData.fields;
+            options.data = $.param(parsedData);
+        }
+    });
+    var to;
     $(document).ajaxComplete(function(event, request, settings) { // trigger after moving tasks
         if(settings && settings.url && settings.url.indexOf('/tfs/DefaultCollection/_apis/git/repositories/') >= 0 && settings.url.indexOf('/pullRequests/') >= 0 && settings.url.indexOf('/workitems') < 0 && request.responseJSON) {
             prDesc = request.responseJSON.description;
         }
-        setTimeout(function() {
+        else if(settings && settings.url && settings.url.indexOf('_apis/wit/workItems') >= 0 && request.responseJSON) {
+            workItemsData = request.responseJSON.value;
+        }
+        clearTimeout(to);
+        to = setTimeout(function() {
             renderMarkdown();
+            renderWorkItemsDetails();
         }, 50);
     });
 }
